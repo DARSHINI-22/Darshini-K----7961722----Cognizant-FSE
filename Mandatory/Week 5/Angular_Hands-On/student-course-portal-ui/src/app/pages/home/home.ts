3f0e1b076cb88a3ad2bddc57397816313185ac83
Darshini - K <darshinikumaran2005@gmail.com>
2026-07-23 16:55:36 +0530

    Angular hands on

--- Mandatory/Week 5/Angular_Hands-On/student-course-portal-ui/src/app/pages/home/home.ts
+++ Mandatory/Week 5/Angular_Hands-On/student-course-portal-ui/src/app/pages/home/home.ts
@@ -1,10 +1,32 @@
+// Property Binding [property] : One-way binding (Component → View)
+// Two-way Binding [(ngModel)] : Data flows both ways (Component ↔ View)
+
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  standalone: true,
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+
+  // Step 11 - String Interpolation
+  portalName = 'Student Course Portal';
+
+  // Step 12 - Property Binding
+  isPortalActive = true;
+
+  // Step 13 - Event Binding
+  message = '';
+
+  // Step 14 - Two-way Binding
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
+}
